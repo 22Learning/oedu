4da9642978718688b1aaee313ae7b2fc2f708339
ouyangqing1996 <1355850294@qq.com>
2020-07-02 00:43:42 +0800

    加入密码输错次数限制，防止暴力破解密码。

--- oedu-login/src/main/java/com/oedu/login/controller/LoginController.java
+++ oedu-login/src/main/java/com/oedu/login/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.oedu.login.controller;
 
 import com.oedu.login.entities.ResponseEntity;
 import com.oedu.login.service.LoginService;
+import com.oedu.login.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,9 @@ public class LoginController {
 
     @GetMapping("login")
     public ResponseEntity logIn(String phone,String password){
+        if(StringUtils.isEmpty(password)){
+            return ResponseEntity.error("密码必填!");
+        }
         return loginService.login(phone,password);
 
     }
--- oedu-login/src/main/java/com/oedu/login/service/LoginService.java
+++ oedu-login/src/main/java/com/oedu/login/service/LoginService.java
@@ -4,34 +4,46 @@ import com.oedu.login.entities.ResponseEntity;
 import com.oedu.login.entities.User;
 import com.oedu.login.mapper.UserMapper;
 import com.oedu.login.utils.MD5Util;
-import com.oedu.login.utils.StringUtils;
-import org.apache.tomcat.util.security.MD5Encoder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
 @Service
+@PropertySource(value = "classpath:/login.properties")
 public class LoginService {
     @Autowired
     UserMapper userMapper;
 
+    @Resource
+    RedisTemplate<String,Integer> redisTemplate;
+    @Value("${login.times:4}")
+    private int loginTimes;
+
     public ResponseEntity<String> login(String phone,String passwd){
-        if(StringUtils.isEmptyOrNull(passwd)) {
-            return ResponseEntity.error("账号或密码错误");
+        String cacheKey = "login_failed_" + phone;
+        //登录次数限制
+        Integer loginFailedTimes = redisTemplate.opsForValue().get(cacheKey);
+        if(null==loginFailedTimes){
+            loginFailedTimes=0;
         }
-
-        String encode = MD5Encoder.encode(passwd.getBytes());
+        if(loginFailedTimes >= loginTimes){
+            return ResponseEntity.error("登录次数达到限制，请5分钟后再试");
+        }
         passwd = MD5Util.getMd5(passwd);
-
         User user = userMapper.getOneUser(phone, passwd);
 
-        System.out.println(user);
         if(null == user){
+            redisTemplate.opsForValue().set(cacheKey,loginFailedTimes+1,5, TimeUnit.MINUTES);
             return ResponseEntity.error("账号或密码错误");
         }
         if(user.getIsEnable() == 0) {
             return ResponseEntity.error("账号已冻结");
         }
-
         return ResponseEntity.success("登陆成功");
     }
 }
